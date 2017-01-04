0047f3c28900ad0696f395b5bcbc0436c7f6afb1
Jan Stegenga <stegenga.j@kpnmail.nl>
2017-01-04 08:33:49 +0100

    Create mi_main.py

---
+++ mi_main.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+'''
+Showcase for several python functionalities:
+- config file
+- logfiles
+- unittests
+- multicore processing (if available)
+- package building
+- applied to process of gaussian elimination of a tridiagonal matrix
+'''
+
+config = configparser.ConfigParser()
+config.read( r"C:\Software\python\distrisense\distrisense.ini" )
+
+class gauss_elim:
+  def __init__():
+    pass
+   
+  def standard():
+    pass
+    
+  def twocores():
+    pass
+
+
+if __name__ == '__main__':
+  pass
