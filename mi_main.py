b2c1b3c392e0acee1f267a54facc84137f129254
Jan Stegenga <stegenga.j@kpnmail.nl>
2017-01-04 11:56:34 +0100

    Update mi_main.py

--- mi_main.py
+++ mi_main.py
@@ -14,15 +14,37 @@ config = configparser.ConfigParser()
 config.read( r"C:\Software\python\distrisense\distrisense.ini" )
 
 class gauss_elim:
-  def __init__():
-    pass
-   
-  def standard():
-    pass
+  def __init__( A = [], a = [], b = [], c = []  ):
+    if A.size > 0:
+      a = np.diag( A, -1 )
+      b = np.diag( A )
+      c = np.diag( A, 1 )
+    self.N = len(b)
+    self.C1[0]   = c[0]/b[0]
+    self.C2[0]   = 1/b[0]
+    for i in range( 1, N ):
+       self.C1[i] = c[i] / ( b[i] - a[i]*C1[i-1] )
+       self.C2[i] = a[i] / ( b[i] - a[i]*C1[i-1] )             
+  
+  def standard( d ):
+    #forward pass:
+    d_prime[0] = d[0]*C2[0]
+    for i, d_elem in enumerate( d[1:] ):
+       d_prime[i] = d[i]
     
   def twocores():
     pass
 
 
 if __name__ == '__main__':
-  pass
+  #simple test case
+  N = 512
+  a = np.ones( N-1 )
+  b = np.ones( N )
+  c = np.ones( N-1 )
+  A = np.diag( a, -1 ) + np.diag( b ) + np.diag( c, +1 )
+  b = np.random.randn( N )
+  
+  #can use timeit, cProfile, pstats for performance
+  x = np.linalg.solve( A, b )
+  
